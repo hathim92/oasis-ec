72db0728151da3553efc8c33c12a2beb50e6618a
Dinesh Kumar <89833532+hathim92@users.noreply.github.com>
2022-04-08 10:43:36 +0530

    delete

--- lib/models/bannerModel.dart
+++
@@ -1,6 +0,0 @@
-class bannerModel {
-  final String Image;
-  bannerModel(this.Image);
-}
-
-List<bannerModel> bannerdata = [bannerModel("images/verticalbanner.jfif")];
--- lib/models/grid_Model.dart
+++
@@ -1,8 +0,0 @@
-class gridProduct {
-  final String Image;
-  final String Title;
-  final String Price;
-  gridProduct(this.Image, this.Title, this.Price);
-}
-
-List<gridProduct> gridData = [];
